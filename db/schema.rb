7d402d43c4c84edf0571c1e39d6ddf486d0ccf2b
Hugo Brook <jhhbrook@gmail.com>
2020-07-15 14:40:53 +0100

    add migrations to drop existing tables then re-add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_133624) do
+ActiveRecord::Schema.define(version: 2020_07_15_133947) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
