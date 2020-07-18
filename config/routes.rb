3b2dc1ac295a4f2bdd04c3c377e9b375b027989d
Hugo Brook <jhhbrook@gmail.com>
2020-07-18 23:43:39 +0100

    starting on session creation

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   resources :users
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
